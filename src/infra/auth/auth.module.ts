a1892d3466e164626ff4ea84d4a0bac6fc0199fb
vmazza <vtrujillomazza@gmail.com>
2025-07-07 20:14:12 -0300

    feat: setup auth module

---
+++ src/infra/auth/auth.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { JwtStrategy } from './jwt.strategy'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtAuthGuard } from './jwt-auth-guard'
+import { EnvModule } from '../env/env.module'
+import { EnvService } from '../env/env.service'
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [EnvModule],
+      inject: [EnvService],
+      global: true,
+      useFactory(env: EnvService) {
+        const jwtSecret = env.get('JWT_SECRET')
+
+        return {
+          signOptions: { algorithm: 'HS256' },
+          secret: jwtSecret,
+        }
+      },
+    }),
+  ],
+  providers: [
+    JwtStrategy,
+    EnvService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+})
+export class AuthModule {}
